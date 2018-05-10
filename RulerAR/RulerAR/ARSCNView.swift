ba49e7df0fc7caab453dcaa2034bec59c145951c
Spencer Cawley <Spent_sir@Spencers-MacBook-Pro-2.local>
2018-05-10 12:27:28 -0600

    added ARSCNView.swift extension file

---
+++ RulerAR/RulerAR/ARSCNView.swift
@@ -0,0 +1,19 @@
+//
+//  ARSCNView.swift
+//  RulerAR
+//
+//  Created by Spencer Cawley on 5/10/18.
+//  Copyright © 2018 Spencer Cawley. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+extension ARSCNView {
+    func realWorldVector(screenPosition: CGPoint) -> SCNVector3? {
+        let results = self.hitTest(screenPosition, types: [.featurePoint])
+        guard let result = results.first else { return nil }
+        return SCNVector3.positionFromTransform(result.worldTransform)
+    }
+}
